831a3e0b2a192185d47bbd320ff8f0b1910acbdc
ye6194 <ye6194@naver.com>
2024-03-02 15:21:14 +0900

    Read까지 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function Header(props) {
+  return (
+    <header>
+      <h1>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
+      </h1>
+    </header>
+  );
+}
+
+function Nav(props) {
+  const lis = [];
+
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+
+    lis.push(
+      <li key={t.id}>
+        {/* t.id는 태그의 속성으로 넘겨졌기 때문에 문자가 됐음 */}
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(Number(event.target.id));
+            /* Number()는 문자를 숫자로 바꿔주는 함수 */
+          }}
+        >
+          {t.title}
+        </a>
+      </li>
+    );
+  }
+
+  return (
+    <nav>
+      <ol>{lis}</ol>
+    </nav>
+  );
+}
+
+function Article(props) {
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
 
 function App() {
+  // const _mode = useState("WELCOME");
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+
+  // 위 세 줄과 같은 코드
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  let content = null;
+
+  const topics = [
+    { id: 1, title: "html", body: "html is..." },
+    { id: 2, title: "css", body: "css is..." },
+    { id: 3, title: "js", body: "js is..." },
+  ];
+
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB"></Article>;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+
+    for (let i = 0; i < topics.length; i++) {
+      console.log(topics[i].id, id);
+      if (topics[i].id === id) {
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+
+    content = <Article title={title} body={body}></Article>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          setMode("WELCOME");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(_id) => {
+          setMode("READ");
+          setId(_id);
+        }}
+      ></Nav>
+      {content}
     </div>
   );
 }
